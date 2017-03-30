1651388aae74044eb4c3a318de826e63858b76f8
mstuart2097 <mstuart@iastate.edu>
2017-03-30 16:40:34 -0500

    Create data.R

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' @title ia_results
+#' @description Total Votes for Trump and Clinton in Iowa by county
+#' A data set containing the raw vote totals for Donald Trump and Hillary Clinton for each of Iowa's 100 counties.
+#' @format A data frame with 100 rows and 3 variables
+#' \describe{
+#'  \item{Vote by county}{County of interest}
+#'  \item{Trump}{Total votes for Donald Trump}
+#'  \item{Clinton}{Total votes for Hillary Clinton}
+#' }
+#' @source \url{http://www.nytimes.com/elections/results/iowa}
