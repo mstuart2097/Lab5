696517a651905e765e3db3cc4f24a9e576f0ad71
Ashish <jain.ashishjain1@gmail.com>
2017-03-30 21:59:26 -0500

    Updated Code

--- R/Lab5Code.R
+++ R/Lab5Code.R
@@ -18,8 +18,8 @@ proportion <- function(Vec1,Vec2,byrow=TRUE){
 
 #' @title Iowa
 #' @description Total Votes for Trump and Clinton in Iowa by county
-#' A data set containing the raw vote totals for Donald Trump and Hillary Clinton for each of Iowa's 100 counties.
-#' @format A data frame with 100 rows and 3 variables
+#' A data set containing the raw vote totals for Donald Trump and Hillary Clinton for each of Iowa's 99 counties.
+#' @format A data frame with 99 rows and 3 variables
 #' \describe{
 #'  \item{Vote by county}{County of interest}
 #'  \item{Trump}{Total votes for Donald Trump}
